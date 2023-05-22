f5f56f29252d7dad350613c88193bbd49bacf76f
Sylwester Arabas <sylwester.arabas@uj.edu.pl>
2023-05-23 01:27:38 +0200

    simplify, make self-check work

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # devops_tests
-pytest test routines asserting for GitHub issue-linked TODO labelling in the code, README link consistency and some Jupyter notebook sanity checks
+pytest test routines asserting for GitHub issue-linked TO-DO labelling in the code, README link consistency and some Jupyter notebook sanity checks
--- test_todos_annotated.py
+++ test_todos_annotated.py
@@ -3,15 +3,8 @@
 
 import os
 import re
-import sys
-import warnings
 
 import pytest
-
-with warnings.catch_warnings():
-    warnings.simplefilter("ignore", category=DeprecationWarning)
-    from fastcore.net import ExceptionsHTTP
-
 from ghapi.all import GhApi, paged
 from git.cmd import Git
 
@@ -41,42 +34,38 @@ def _git_tracked_file(request):
 @pytest.fixture(scope="session", name="gh_issues")
 def _gh_issues():
     res = {}
-    if "CI" not in os.environ or (
-        "GITHUB_ACTIONS" in os.environ and sys.version_info.minor >= 8
-    ):
-        repo = os.path.basename(Git(".").rev_parse("--show-toplevel"))
-        try:
-            api = GhApi(owner="open-atmos", repo=repo)
-            pages = paged(
-                api.issues.list_for_repo,
-                owner="open-atmos",
-                repo=repo,
-                state="all",
-                per_page=100,
-            )
-            for page in pages:
-                for item in page.items:
-                    res[item.number] = item.state
-        except ExceptionsHTTP[403]:
-            pass
+    repo = os.path.basename(Git(".").rev_parse("--show-toplevel"))
+    api = GhApi(owner="open-atmos", repo=repo)
+    pages = paged(
+        api.issues.list_for_repo,
+        owner="open-atmos",
+        repo=repo,
+        state="all",
+        per_page=100,
+    )
+    for page in pages:
+        for item in page.items:
+            res[item.number] = item.state
     return res
 
 
 def test_todos_annotated(git_tracked_file, gh_issues):
-    """raises assertion errors if a TODO or FIXME is not annotated of if the annotation
+    """raises assertion errors if a (TODO|FIXME) is not annotated or if the annotation
     does not point to an open issue"""
     for line in _grep(git_tracked_file, r".*(TODO|FIXME).*"):
+        if "(TODO|FIXME)" in line:
+            continue
         match = re.search(r"(TODO|FIXME) #(\d+)", line)
         if match is None:
-            raise AssertionError(f"TODO/FIXME not annotated with issue id ({line})")
+            raise AssertionError(f"(TODO|FIXME) not annotated with issue id ({line})")
         giving_up_with_hope_other_builds_did_it = len(gh_issues) == 0
         if not giving_up_with_hope_other_builds_did_it:
             number = int(match.group(2))
             if number not in gh_issues.keys():
                 raise AssertionError(
-                    f"TODO/FIXME annotated with non-existent id ({line})"
+                    f"(TODO|FIXME) annotated with non-existent id ({line})"
                 )
             if gh_issues[number] != "open":
                 raise AssertionError(
-                    f"TODO/FIXME remains for a non-open issue ({line})"
+                    f"(TODO|FIXME) remains for a non-open issue ({line})"
                 )
