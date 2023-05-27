6415525926bf88737289ceb91f93795b325602f4
Sylwester Arabas <sylwester.arabas@uj.edu.pl>
2023-05-28 00:22:05 +0200

    beef-ups from PyPartMC-examples

--- test_run_notebooks.py
+++ test_run_notebooks.py
@@ -8,6 +8,10 @@ if sys.platform == "win32" and sys.version_info[:2] >= (3, 7):
 
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
+import gc
+import os
+import warnings
+
 import nbformat
 import pytest
 from git.cmd import Git
@@ -28,7 +32,18 @@ def _notebook_filename(request):
 
 def test_run_notebooks(notebook_filename, tmp_path):
     """executes a given notebook"""
+    os.environ["JUPYTER_PLATFORM_DIRS"] = "1"
+
+    executor = ExecutePreprocessor(timeout=15 * 60, kernel_name="python3")
+
     with open(notebook_filename, encoding="utf8") as notebook_file:
-        ExecutePreprocessor(timeout=15 * 60, kernel_name="python3").preprocess(
-            nbformat.read(notebook_file, as_version=4), {"metadata": {"path": tmp_path}}
-        )
+        # https://github.com/pytest-dev/pytest-asyncio/issues/212
+        with warnings.catch_warnings():
+            warnings.filterwarnings("ignore", message="There is no current event loop")
+            executor.preprocess(
+                nbformat.read(notebook_file, as_version=4),
+                {"metadata": {"path": tmp_path}},
+            )
+
+    # so that nbconvert perplexities are reported here, and not at some dtor test later on
+    gc.collect()
