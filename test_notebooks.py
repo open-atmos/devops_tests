ede0dd780a0ae36fb99b6b1f388714867f03a3b1
Oleksii Bulenok <oleksii.bulenok@swmansion.com>
2023-10-23 01:13:29 +0200

    Move set_event_loop_policy hack up

--- test_notebooks.py
+++ test_notebooks.py
@@ -1,9 +1,15 @@
 """ executes all Jupyter notebooks tracked by git """
 # pylint: disable=wrong-import-position
 # https://bugs.python.org/issue37373
+import sys
+
+if sys.platform == "win32" and sys.version_info[:2] >= (3, 7):
+    import asyncio
+
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
 import gc
 import os
-import sys
 import warnings
 
 import nbformat
@@ -11,11 +17,6 @@ import pint
 import pytest
 
 from utils import find_files
-
-if sys.platform == "win32" and sys.version_info[:2] >= (3, 7):
-    import asyncio
-
-    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
 
 with warnings.catch_warnings():
