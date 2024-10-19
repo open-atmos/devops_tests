488d111097fff3775768de6507ab1a7575a7cb30
AgnieszkaZaba <56157996+AgnieszkaZaba@users.noreply.github.com>
2024-10-19 12:41:35 +0200

    add check if all notebooks have md with badges, md with description and Colab header (#34)

--- test_notebooks.py
+++ test_notebooks.py
@@ -11,6 +11,7 @@ if sys.platform == "win32" and sys.version_info[:2] >= (3, 7):
 
 import gc
 import os
+import pathlib
 import warnings
 
 import nbformat
@@ -24,6 +25,28 @@ with warnings.catch_warnings():
     from nbconvert.preprocessors import ExecutePreprocessor
 
 SI = pint.UnitRegistry()
+
+
+def _relative_path(absolute_path):
+    relpath = os.path.relpath(absolute_path, _repo_path().absolute())
+    posixpath_to_make_it_usable_in_urls_even_on_windows = pathlib.Path(
+        relpath
+    ).as_posix()
+    return posixpath_to_make_it_usable_in_urls_even_on_windows
+
+
+def _repo_path():
+    path = pathlib.Path(__file__)
+    while not (path.is_dir() and (path / ".git").exists()):
+        path = path.parent
+    return path
+
+
+COLAB_HEADER = f"""import sys
+if 'google.colab' in sys.modules:
+    !pip --quiet install open-atmos-jupyter-utils
+    from open_atmos_jupyter_utils import pip_install_on_colab
+    pip_install_on_colab('{_repo_path().name}-examples')"""
 
 
 @pytest.fixture(
@@ -83,3 +106,65 @@ def test_jetbrains_bug_py_66491(notebook_filename):
                     + " (could be due to a bug in PyCharm,"
                     + " see https://youtrack.jetbrains.com/issue/PY-66491 )"
                 )
+
+
+def _preview_badge_markdown(absolute_path):
+    svg_badge_url = (
+        "https://img.shields.io/static/v1?"
+        + "label=render%20on&logo=github&color=87ce3e&message=GitHub"
+    )
+    link = (
+        f"https://github.com/open-atmos/{_repo_path().name}/blob/main/"
+        + f"{_relative_path(absolute_path)}"
+    )
+    return f"[![preview notebook]({svg_badge_url})]({link})"
+
+
+def _mybinder_badge_markdown(abslute_path):
+    svg_badge_url = "https://mybinder.org/badge_logo.svg"
+    link = (
+        f"https://mybinder.org/v2/gh/open-atmos/{_repo_path().name}.git/main?urlpath=lab/tree/"
+        + f"{_relative_path(abslute_path)}"
+    )
+    return f"[![launch on mybinder.org]({svg_badge_url})]({link})"
+
+
+def _colab_badge_markdown(absolute_path):
+    svg_badge_url = "https://colab.research.google.com/assets/colab-badge.svg"
+    link = (
+        f"https://colab.research.google.com/github/open-atmos/{_repo_path().name}/blob/main/"
+        + f"{_relative_path(absolute_path)}"
+    )
+    return f"[![launch on Colab]({svg_badge_url})]({link})"
+
+
+def test_first_cell_contains_three_badges(notebook_filename):
+    """checks if all notebooks feature Github preview, mybinder and Colab badges
+    (in the first cell)"""
+    with open(notebook_filename, encoding="utf8") as fp:
+        nb = nbformat.read(fp, nbformat.NO_CONVERT)
+        assert len(nb.cells) > 0
+        assert nb.cells[0].cell_type == "markdown"
+        lines = nb.cells[0].source.split("\n")
+        assert len(lines) == 3
+        assert lines[0] == _preview_badge_markdown(notebook_filename)
+        assert lines[1] == _mybinder_badge_markdown(notebook_filename)
+        assert lines[2] == _colab_badge_markdown(notebook_filename)
+
+
+def test_second_cell_is_a_markdown_cell(notebook_filename):
+    """checks if all notebooks have their second cell with some markdown
+    (hopefully clarifying what the example is about)"""
+    with open(notebook_filename, encoding="utf8") as fp:
+        nb = nbformat.read(fp, nbformat.NO_CONVERT)
+        assert len(nb.cells) > 1
+        assert nb.cells[1].cell_type == "markdown"
+
+
+def test_third_cell_contains_colab_header(notebook_filename):
+    """checks if all notebooks feature a Colab-magic cell"""
+    with open(notebook_filename, encoding="utf8") as fp:
+        nb = nbformat.read(fp, nbformat.NO_CONVERT)
+        assert len(nb.cells) > 2
+        assert nb.cells[2].cell_type == "code"
+        assert nb.cells[2].source == COLAB_HEADER
