80a3baf95cc03004591a60ed355c972fbe4495ec
sfonxu <wronski.michal4@wp.pl>
2025-09-15 10:42:47 +0200

    squash! cleanup

--- test_notebooks.py
+++ test_notebooks.py
@@ -51,23 +51,3 @@ def test_run_notebooks(notebook_filename, tmp_path):
 
     # so that nbconvert perplexities are reported here, and not at some dtor test later on
     gc.collect()
-
-
-def test_file_size(notebook_filename):
-    """checks if all example Jupyter notebooks have file size less than an arbitrary limit"""
-    assert os.stat(notebook_filename).st_size * SI.byte < 2 * SI.megabyte
-
-
-def test_jetbrains_bug_py_66491(notebook_filename):
-    """checks if all notebooks have the execution_count key for each cell in JSON what is
-    required by GitHub renderer and what happens not to be the case if generating the notebook
-    using buggy versions of PyCharm: https://youtrack.jetbrains.com/issue/PY-66491"""
-    with open(notebook_filename, encoding="utf8") as notebook_file:
-        notebook = nbformat.read(notebook_file, nbformat.NO_CONVERT)
-        for cell in notebook.cells:
-            if cell.cell_type == "code" and not hasattr(cell, "execution_count"):
-                raise AssertionError(
-                    "notebook cell is missing the execution_count attribute"
-                    + " (could be due to a bug in PyCharm,"
-                    + " see https://youtrack.jetbrains.com/issue/PY-66491 )"
-                )
